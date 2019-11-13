d331df76c0c9a59317d88893ac9c638e213b7dd5
John C. Burnham <jcb@johnchandlerburnham.com>
2019-11-13 18:40:00 -0500

    update numeric operations to align with wasm primitives

--- FM-Net/fm-net.c
+++ FM-Net/fm-net.c
@@ -1,6 +1,7 @@
 #include <assert.h>
 #include <math.h>
 #include <stdint.h>
+#include <limits.h>
 
 #include "fm-net.h"
 
@@ -18,30 +19,82 @@ enum {
 };
 
 enum {
-  ADD,
-  SUB,
-  MUL,
-  DIV,
-  MOD,
-  POW,
-  AND,
-  BOR,
-  XOR,
-  NOT,
-  SHR,
-  SHL,
-  GTR,
-  LES,
-  EQL,
-  FADD,
-  FSUB,
-  FMUL,
-  FDIV,
-  FMOD,
-  FPOW,
-  ITOF,
-  FTOI,
+  EQ,      // equality
+  NE,      // not-equal
+  LT_S,    // signed less-than
+  LT_U,    // unsigned less-than
+  GT_S,    // signed greater-than
+  GT_U,    // unsigned greater-than
+  LE_S,    // signed less-than-or-equal
+  LE_U,    // unsigned less-than-or-equal
+  GE_S,    // signed greater-than-or-equal
+  GE_U,    // unsigned greater-than-or-equal
+  CLZ,     // count leading zeros, unary
+  CTZ,     // count trailing zeros, unary
+  POPCNT,  // count number of 1 bits, unary
+  SHL,     // shift left
+  SHR,     // unsigned shift right
+  SHR_S,   // signed shift right
+  ROTL,    // rotate left
+  ROTR,    // rotate right
+  AND,     // bitwise and
+  OR,      // bitwise or
+  XOR,     // bitwise xor
+  ADD,     // addition
+  SUB,     // subtraction
+  MUL,     // multiplication
+  DIV_S,   // signed division
+  DIV_U,   // unsigned division
+  REM_S,   // signed remainder
+  REM_U,   // unsigned remainder
+  FABS,    // absolute value, unary
+  FNEG,    // negation, unary
+  FCEIL,   // round upward, unary
+  FFLOOR,  // round downward, unary
+  FTRUNC,  // truncate, unary
+  FNRST,   // round to nearest, unary
+  FSQRT,   // square-root, unary
+  FADD,    // addition
+  FSUB,    // subtraction
+  FMUL,    // multiplication
+  FDIV,    // division
+  FMIN,    // minimum
+  FMAX,    // maximum
+  FCPYSGN, // copy sign value of first arg and sign of second arg
+  FEQ,     // equality
+  FNE,     // not-equal
+  FLT,     // less-than
+  FGT,     // greater-than
+  FLE,     // less-than-or-equal
+  FGE,     // greater-than-or-equal
+  EXT32_S, // signed extension of 32 bit numbers to 64 bit
+  FTOS,    // f64 to signed i64
+  FTOU,    // f64 to unsigned i64
+  STOF,    // signed i64 to f64
+  UTOF     // UNSIGNED I64 TO F64
 };
+
+uint64_t rotl(uint64_t a, uint64_t b) {
+  const uint64_t mask = CHAR_BIT*sizeof(b) - 1;
+  b &= mask;
+  return (a << b) | (a >> ((-b) & mask));
+}
+
+uint64_t rotr(uint64_t a, uint64_t b) {
+  const uint64_t mask = CHAR_BIT*sizeof(b) - 1;
+  b &= mask;
+  return (a >> b) | (a << ((-b) & 63));
+}
+
+//static uint64_t powi(uint64_t fst, uint64_t snd) {
+//  uint64_t res;
+//
+//  for (res = 1; snd; snd >>= 1, fst *= fst) {
+//    if (snd & 1)
+//      res *= fst;
+//  }
+//  return res;
+//}
 
 static uint64_t alloc_node(Net *net) {
   uint64_t addr;
@@ -65,20 +118,11 @@ static void queue(Net *net, uint64_t addr) {
   assert(addr % 4 == 0);
 }
 
-static uint64_t powi(uint64_t fst, uint64_t snd) {
-  uint64_t res;
-
-  for (res = 1; snd; snd >>= 1, fst *= fst) {
-    if (snd & 1)
-      res *= fst;
-  }
-  return res;
-}
-
 static void rewrite(Net *net, uint64_t a_addr) {
   uint64_t *nodes = net->nodes;
   union {
-    uint64_t i;
+    uint64_t u;
+    int64_t  s;
     double f;
   } res, fst, snd;
   uint64_t b_addr, c_addr, d_addr;
@@ -91,39 +135,70 @@ static void rewrite(Net *net, uint64_t a_addr) {
 
   switch (a_info & 3) {
   case NUM:
-    fst.i = a[0];
+    fst.u = a[0];
     switch (a_kind) {
     case OP1:
-      snd.i = a[1];
-      switch (a[3] >> 8) {
-      case ADD: res.i = fst.i + snd.i; break;
-      case SUB: res.i = fst.i - snd.i; break;
-      case MUL: res.i = fst.i * snd.i; break;
-      case DIV: res.i = fst.i / snd.i; break;
-      case MOD: res.i = fst.i % snd.i; break;
-      case POW: res.i = powi(fst.i, snd.i); break;
-      case AND: res.i = fst.i & snd.i; break;
-      case BOR: res.i = fst.i | snd.i; break;
-      case XOR: res.i = fst.i ^ snd.i; break;
-      case NOT: res.i = ~snd.i; break;
-      case SHR: res.i = fst.i >> snd.i; break;
-      case SHL: res.i = fst.i << snd.i; break;
-      case GTR: res.i = fst.i > snd.i; break;
-      case LES: res.i = fst.i < snd.i; break;
-      case EQL: res.i = fst.i == snd.i; break;
-      case FADD: res.f = fst.f + snd.f; break;
-      case FSUB: res.f = fst.f - snd.f; break;
-      case FMUL: res.f = fst.f * snd.f; break;
-      case FDIV: res.f = fst.f / snd.f; break;
-      case FMOD: res.f = fmod(fst.f, snd.f); break;
-      case FPOW: res.f = pow(fst.f, snd.f); break;
-      case ITOF: res.f = snd.i; break;
-      case FTOI: res.i = snd.f; break;
+      snd.u = a[1];
+    switch (a[3] >> 8) {
+      case EQ:      res.u = fst.u == snd.u; break;
+      case NE:      res.u = fst.u != snd.u; break;
+      case LT_S:    res.s = fst.s < snd.s; break;
+      case LT_U:    res.u = fst.u < snd.u; break;
+      case GT_S:    res.s = fst.s > snd.s; break;
+      case GT_U:    res.u = fst.u > snd.u; break;
+      case LE_S:    res.u = fst.s <= snd.s; break;
+      case LE_U:    res.u = fst.u <= snd.u; break;
+      case GE_S:    res.u = fst.u >= snd.u; break;
+      case GE_U:    res.u = fst.u >= snd.u; break;
+      case CLZ:     res.u = __builtin_clz(snd.u); break;
+      case CTZ:     res.u = __builtin_ctz(snd.u); break;
+      case POPCNT:  res.u = __builtin_popcount(snd.u); break;
+      case SHL:     res.u = fst.u << snd.u; break;
+      case SHR:     res.u = fst.u >> snd.u; break;
+      case SHR_S:   res.s = fst.s >> snd.u; break;
+      case ROTL:    res.u = rotl(fst.u, snd.u); break;
+      case ROTR:    res.u = rotr(fst.u, snd.u); break;
+      case AND:     res.u = fst.u & snd.u; break;
+      case  OR:     res.u = fst.u | snd.u; break;
+      case XOR:     res.u = fst.u ^ snd.u; break;
+      case ADD:     res.u = fst.u + snd.u; break;
+      case SUB:     res.u = fst.u - snd.u; break;
+      case MUL:     res.u = fst.u * snd.u; break;
+      case DIV_S:   res.s = fst.s / snd.s; break;
+      case DIV_U:   res.u = fst.u / snd.u; break;
+      case REM_S:   res.s = fst.s % snd.s; break;
+      case REM_U:   res.u = fst.u % snd.u; break;
+      case FABS:    res.f = fabs(snd.f); break;
+      case FNEG:    res.f = -1 * snd.f; break;
+      case FCEIL:   res.f = ceil(snd.f); break;
+      case FFLOOR:  res.f = floor(snd.f); break;
+      case FTRUNC:  res.f = trunc(snd.f); break;
+      case FNRST:   res.f = round(snd.f); break;
+      case FSQRT:   res.f = sqrt(snd.f); break;
+      case FADD:    res.f = fst.f + snd.f; break;
+      case FSUB:    res.f = fst.f - snd.f; break;
+      case FMUL:    res.f = fst.f * snd.f; break;
+      case FDIV:    res.f = fst.f / snd.f; break;
+      case FMIN:    res.f = fmin(fst.f, snd.f); break;
+      case FMAX:    res.f = fmax(fst.f, snd.f); break;
+      case FCPYSGN: res.f = copysign(fst.f, snd.f); break;
+      case FEQ:     res.u = fst.f == snd.f; break;
+      case FNE:     res.u = isunordered(fst.f,snd.f) ? 1 :
+                              islessgreater(fst.f, snd.f); break;
+      case FLT:     res.u = isless(fst.f, snd.f); break;
+      case FGT:     res.u = isgreater(fst.f, snd.f); break;
+      case FLE:     res.u = islessequal(fst.f, snd.f); break;
+      case FGE:     res.u = isgreaterequal(fst.f, snd.f); break;
+      //case EXT32_S:
+      case FTOS:   res.s = snd.f; break;
+      case FTOU:   res.u = snd.f; break;
+      case STOF:   res.f = snd.s; break;
+      case UTOF:   res.f = snd.u; break;
       /* unreachable */
-      default: res.i = 0; break;
+      default: res.u = 0; break;
       }
       if ((a[3] >> 4 & 3) == PTR) {
-        nodes[a[2]] = res.i;
+        nodes[a[2]] = res.u;
         nodes[a[2] | 3] |= NUM << (a[2] & 3) * 2;
         if ((a[2] & 3) == 0)
           queue(net, a[2]);
@@ -132,7 +207,7 @@ static void rewrite(Net *net, uint64_t a_addr) {
       break;
     case OP2:
       a[0] = a[1];
-      a[1] = fst.i;
+      a[1] = fst.u;
       a[3] = OP1 << 6 | NUM << 2 | (a[3] >> 2 & 3) |
              (a[3] & ~(3 << 6 | 3 << 0 | 3 << 2));
       if ((a[3] & 3) == PTR)
@@ -142,13 +217,13 @@ static void rewrite(Net *net, uint64_t a_addr) {
       break;
     case NOD:
       if ((a[3] >> 2 & 3) == PTR) {
-        nodes[a[1]] = fst.i;
+        nodes[a[1]] = fst.u;
         nodes[a[1] | 3] |= NUM << (a[1] & 3) * 2;
         if ((a[1] & 3) == 0)
           queue(net, a[1]);
       }
       if ((a[3] >> 4 & 3) == PTR) {
-        nodes[a[2]] = fst.i;
+        nodes[a[2]] = fst.u;
         nodes[a[2] | 3] |= NUM << (a[2] & 3) * 2;
         if ((a[2] & 3) == 0)
           queue(net, a[2]);
@@ -160,7 +235,7 @@ static void rewrite(Net *net, uint64_t a_addr) {
       a[0] = a[1];
       a[3] = NOD << 6 | (a[3] >> 2 & 3) | (a[3] & ~0xff);
       nodes[a[0]] &= ~3;
-      if (fst.i) {
+      if (fst.u) {
         a[1] = a[2];
         a[3] |= (a_info >> 2 & 3 << 2) | ERA << 4;
         nodes[a[1]] ^= 3;
